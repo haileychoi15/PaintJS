1dd59a5a1b52261f7dae1bb7cd221ecdb0f56df3
yuyeong choi <imdud0612@gmail.com>
2020-09-24 01:17:18 +0900

    color change 기능

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('jsCanvas');
+    const canvas = document.querySelector('#jsCanvas');
+    const ctx = canvas.getContext('2d');
+    const colors = document.querySelector('#jsColors');
+
+    const canvasStyle = window.getComputedStyle(canvas);
+    canvas.width = Number(canvasStyle.width.replace('px', ''));
+    canvas.height = Number(canvasStyle.height.replace('px', ''));
+
+    ctx.strokeStyle = '#00000';
+    ctx.lineWidth = 2.5;
+
     let painting = false;
     function onMouseMove(e) {
         const x = e.offsetX;
         const y = e.offsetY;
+        if (!painting) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
     }
 
     function stopPainting() {
         painting = false;
     }
-    
-    function onMouseDown(e) {
+
+    function startPainting() {
         painting = true;
-        console.log(e);
+
     }
 
-    function onMouseUp(e) {
-        stopPainting();
+    function handleColorClick(e) {
+        if(e.target.nodeName === 'LI') {
+            const color = window.getComputedStyle(e.target).backgroundColor;
+            ctx.strokeStyle = color;
+        }
     }
 
     if (canvas) {
         canvas.addEventListener('mousemove', onMouseMove);
-        canvas.addEventListener('mousedown', onMouseDown);
-        canvas.addEventListener('mouseup', onMouseUp);
+        canvas.addEventListener('mousedown', startPainting);
+        canvas.addEventListener('mouseup', stopPainting);
         canvas.addEventListener('mouseleave', stopPainting);
     }
+
+    colors.addEventListener('click', handleColorClick);
+
 
 
 
